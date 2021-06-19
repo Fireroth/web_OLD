09e1209b98a60984f87d0f9283b074985c416f9b
Fireroth <70514831+Fireroth@users.noreply.github.com>
2021-06-19 23:21:34 +0200

    Delete temp/command-menu/resources directory

--- temp/command-menu/resources/matrix.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-mode con cols=120 lines=30
-title Matrix effect
-color 0a
-:rn
-echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-goto rn
